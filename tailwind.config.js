9dcfb231e2efcb3ea7d36791af7363f9457d8395
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-04-25 15:35:19 +0100

    Header - Desktop verion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./**/*{.html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
     fontFamily: {
       'main': ['Montserrat', 'sans-serif'],
     },
