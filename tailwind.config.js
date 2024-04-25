73dc2d3a1da5ff84f2e03e4e9e1520b489c3f524
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-04-25 12:25:43 +0100

    Add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*{.html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'main': ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'clr-background': '#F4F4F4',
+        'clr-primary': '#3F51B5',
+        'clr-hover': '#1976D2',
+        'clr-text': '#333333',
+      },
+    },
   },
   plugins: [],
 }
