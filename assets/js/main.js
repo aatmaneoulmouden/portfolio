1c64cd7ff61fc889cf76664e409c2b67941e5acf
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-15 22:38:46 +0100

    Projects section version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// Projects Swiper
+var swiper = new Swiper(".projectSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+    navigation: {
+      nextEl: ".project-swiper-button-next",
+      prevEl: ".project-swiper-button-prev",
+    },
+});
